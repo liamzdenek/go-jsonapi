7dc406a23d409d5bbcaf15c0d51d0e656dc4fe97
Liam Zdenek <liamzdenek@gmail.com>
2015-06-20 14:53:17 -0500

    Removing outdated docs on example

--- example/main.go
+++ example/main.go
@@ -63,29 +63,6 @@ func main() {
     api.MountResource("comment", resource_comment, rbac.Require("canComment"));
     api.MountResource("login", resource_login);
 
-    // Relationships are the other large concept in addition to Resources. Each relationship
-    // is a unidirectional associations between two resources. The relationship is given a name--
-    // in the following example, the relationship name is "logged_in_as", and it associates a given
-    // "session" with a "user." 
-    //
-    // Relationships have a Relationship Behavior, which is responsible for taking any arbitrary
-    // record of the source resource, and converting it into the destination resource. There are
-    // no behaviors built in to core, but there are a few in jsonapi/extras
-    //
-    // Finally, relationships have an authenticator, which can refuse access upon any arbitrary
-    // conditions, just like Resources TODO: this isn't totally working as desired yet, so use with caution
-    // Example requests (note, these are not exact responses as they have been simplified to exaggerate mechanics):
-    // * curl -X GET "localhost:3030/session/1/logged_in_as"
-    //   * returns: {"data":{"type":"user","id":"1","attributes":{"name":"Jsonapi IsGreat"}}}
-    //
-    // * curl -X GET "localhost:3030/session/1/links/logged_in_as
-    //   * returns: {"data":{"type":"user","id":"1"}}
-    /*
-    api.MountRelationship("logged_in_as", "session", // name, src, dest
-        NewRelationshipFromFieldToId("user", "UserId", Required),
-        no_auth,
-    );
-    */
     api.MountRelationship("posts", "user",
         relationship.NewFromFieldToField("post", "ID", "UserId", NotRequired),
         no_auth,
@@ -98,14 +75,6 @@ func main() {
         relationship.NewFromFieldToField("comment", "ID", "PostId", Required),
         no_auth,
     );
-
-    // For requests with a resource as the primary "data" (aka, not a /:resource/:id/links/:linkname request),
-    // you may specify ?include=[includefmt] to include the data for additional links. For example:
-    //
-    // * curl -X GET "localhost:3030/session/1/logged_in_as?include=posts,posts.comments
-    //   * includes the additional field "includes" populated with all the data needed to navigate through the links.
-
-    // For guided documentation, see the 'docs' folder
 
     // that's it! start the API
     fmt.Printf("Listening\n");
